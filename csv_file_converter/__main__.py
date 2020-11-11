c2608c2ddbd9a3bf9e68df223dac6c632c7bafd1
annakallo <anna.kallo@gmail.com>
2020-11-11 18:18:32 +0200

    CSV converter: printing out the names what you give.

--- csv_file_converter/__main__.py
+++ csv_file_converter/__main__.py
@@ -8,9 +8,9 @@ def main():
     print('The directory contains the facturas-iva-desglosado.csv, you can import this file to be converted or add '
           'another csv file to the directory and import that.')
     import_file_name = input('Enter the import filename: ')
-    # print('You entered: ' + import_file_name)
+    print('You entered: ' + import_file_name)
     export_file_name = input('Name the newly created export file: ')
-    # print('You entered: ' + export_file_name)
+    print('You entered: ' + export_file_name)
 
     import_data = csv_converter.import_csv_original_invoices(import_file_name)
 
